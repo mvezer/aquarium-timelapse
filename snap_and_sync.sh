4e04a7378c862de475e91222ca62ce3edd853dbd
Matyas Vezer <vezer.m@gmail.com>
2020-09-11 20:41:10 +0200

    add snap and sync automatic folder names

--- snap_and_sync.sh
+++ snap_and_sync.sh
@@ -1,3 +1,3 @@
 raspistill -q 98 -n -t 1000 -ts -o /home/pi/timelapse/image_%d.jpg -w 3840 -h 2160 \
-&& rclone copy /home/pi/timelapse dropbox:timelapse \
+&& rclone copy /home/pi/timelapse google:timelapse/$(date +%Y)-week#$(date +%V) \
 && rm /home/pi/timelapse/*
